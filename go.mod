6ff6989b014749b46409eb9f3135f7bfd4e112fa
alfauzan003 <habibalfauzan1@gmail.com>
2023-09-21 06:05:39 +0700

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module weather-tracker
+
+go 1.21.1
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+type apiConfigData struct {
+	OpenWeatherMapApiKey string `json:"OpenWeatherMapApiKey"`
+}
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
+
+func loadApiConfig(filename string) (apiConfigData, error) {
+	bytes, err := os.ReadFile(filename)
+
+	if err != nil {
+		return apiConfigData{}, err
+	}
+	var c apiConfigData
+
+	err = json.Unmarshal(bytes, &c)
+	if err != nil {
+		return apiConfigData{}, err
+	}
+	return c, nil
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello!"))
+}
+
+func query(city string) (weatherData, error) {
+	apiConfig, err := loadApiConfig(".apiConfig")
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	resp, err := http.Get("https://api.openweathermap.org/data/2.5/weather?&q=" + city + "&appid=" + apiConfig.OpenWeatherMapApiKey)
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var d weatherData
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+	return d, nil
+}
+
+func city(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	city := vars["city"]
+	data, err := query(city)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(data)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", hello)
+	r.HandleFunc("/weather/{city}", city)
+
+	fmt.Printf("Start server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
